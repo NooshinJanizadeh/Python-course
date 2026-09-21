e185d45b015402522d93ab1a1b70c86f4d11ae77
Nooshin <nooshin.janizadeh@gmail.com>
2026-09-22 01:01:33 +0330

    add task2

---
+++ task2.py
@@ -0,0 +1,8 @@
+num=10
+text="hello"
+f=10.5
+t=True
+print("num=",num,"type(num)=", type(num))
+print("text=",text,"type(text)=", type(text))
+print("f=",f,"type(f)=", type(f))
+print("t=",t,"type(t)=", type(t))
