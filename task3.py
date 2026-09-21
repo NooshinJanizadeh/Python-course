82832c603f173a00392f23aac9f5cd6edabec851
Nooshin <nooshin.janizadeh@gmail.com>
2026-09-22 01:19:58 +0330

    modify task3

--- task3.py
+++ task3.py
@@ -4,5 +4,14 @@ st="123"
 t="False"
 print("n=",float(n))
 print("f=",int(f))
+print("n=",str(n))
 print("st=",int(st))
-print("t=", str(t))
+print("n=", bool(n))
+
+# calculate factorial
+num=int (input("enter a number="))
+fa=1
+for i in range(1,num+1):
+    fa=fa*i
+print("factorial of =",num, "is=",fa)
+
